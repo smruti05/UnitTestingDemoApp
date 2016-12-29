25d8dffcdca107b733e4e7e552694dd00146b74c
smruti <smrutimate@gmail.com>
2016-12-30 01:02:31 +0530

    corrections

---
+++ app/src/test/java/com/unittestingdemoapp/DateTimeConverterTest.java
@@ -0,0 +1,35 @@
+package com.unittestingdemoapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Calendar;
+
+public class DateTimeConverterTest {
+    Calendar calendar = Calendar.getInstance();
+
+    @Test
+    public void convert() {
+        Assert.assertEquals("Positive test case - update date with current date", "30th Dec 2016", DateTimeConverter.convertDateTime(calendar.getTime()));
+    }
+
+    @Test
+    public void check() {
+        Assert.assertNotNull("Positive test case", DateTimeConverter.convertDateTime(calendar.getTime()));
+    }
+
+    @Test
+    public void check_isNotToday() {
+        Assert.assertNotEquals("Positive test case", "today", DateTimeConverter.convertDateTime(calendar.getTime()));
+    }
+
+    @Test
+    public void check_isToday() {
+        Assert.assertSame("Negative test case", "27/12/2016", DateTimeConverter.convertDateTime(calendar.getTime()));
+    }
+
+    @Test
+    public void check_isNull() {
+        Assert.assertNotNull("Negative test case - Pass the null value to convert()", DateTimeConverter.convertDateTime(null));
+    }
+}
